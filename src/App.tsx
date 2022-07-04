696afeb2d639dcc52428cb0f8ccdc6ed8dd66a66
dCieszynski <dCieszynski99@gmail.com>
2022-07-04 17:27:19 -0500

    add credintial access

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Buffer } from "buffer";
+import List from "./components/List";
 
+export interface AircraftData {
+  icao24: string;
+  callsign: string;
+  originCountry: string;
+  timePosition: number | null;
+  lastContact: number;
+  longitude: number;
+  latitude: number;
+  baroAltitude: number | null;
+  onGround: boolean;
+  velocity: number;
+  trueTrack: number | null;
+  verticalRate: number | null;
+  sensors: null;
+  geoAltitude: number | null;
+  squawk: string | null;
+  spi: boolean;
+  positionSource: number;
+  category: number;
+}
 
 function App() {
-  const [aircrafts, setAircrafts] = useState([]);
+  const [aircrafts, setAircrafts] = useState<AircraftData[]>([]);
 
   useEffect(() => {
-    const getAircratsData = async () => {
-      const url = "https://opensky-network.org/api/states/all";
-      const params = new URLSearchParams({ lamin: "49.00",
-      lomin: "14.07",
-      lamax: "54.50",
-      lomax:"24.09"});
+    const getAircratsData = async (): Promise<void> => {
+      const url = "https://@opensky-network.org/api/states/all";
+      const headers = new Headers();
+      const username = "VampusDC";
+      const password = "Dawidwsn12!"
+      headers.append("Authorization", "Basic " + window.btoa(username + ":" + password))
+      const params = new URLSearchParams({
+        lamin: "49.00",
+        lomin: "14.07",
+        lamax: "54.50",
+        lomax: "24.09",
+      });
 
-      const response = await fetch(`${url}?${params.toString()}`);
+      const response = await fetch(`${url}?${params.toString()}`, {headers: headers});
       const data = await response.json();
-      const {states} = await data;
+      console.log(data);
+      const { states } = await data;
       if (states.length > 0) {
         setAircrafts(states);
       }
-    }
+    };
 
-    const interval = setInterval(() => getAircratsData(), 30000);
+    const interval = setInterval(() => getAircratsData(), 15000);
     console.log(aircrafts);
     return () => clearInterval(interval);
+  }, [aircrafts]);
 
-  }, [aircrafts])
-
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App">Skytracker</div>;
 }
 
 export default App;
