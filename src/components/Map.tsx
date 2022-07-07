156d816993997d92d9f1d44d3adec9a3f9c195b8
dCieszynski <dCieszynski99@gmail.com>
2022-07-07 11:20:58 -0500

    add aircraft icon

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import Leaflet from "leaflet"
+import { MapContainer, TileLayer, useMap, Marker} from 'react-leaflet'
 import { AircraftData } from '../App'
 
 interface Props {
   aircrafts: AircraftData[]
 }
 
+let myIcon = Leaflet.icon({
+  iconUrl: require("../assets/aircraft.png"),
+  iconSize: [20, 20],
+  iconAnchor: [10, 10],
 
+});
 
 const Map: React.FC<Props> = ({aircrafts}) => {
   console.log(aircrafts)
@@ -14,7 +20,7 @@ const Map: React.FC<Props> = ({aircrafts}) => {
     <MapContainer center={[52.12, 19.00]} zoom={8} scrollWheelZoom={false} style={{height: "280px"}}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
         {aircrafts.map(aircraft => {
-          return <Marker position={[aircraft.latitude, aircraft.longitude]}></Marker>
+          return <Marker position={[aircraft.latitude, aircraft.longitude]} icon={myIcon}></Marker>
         })}
     </MapContainer>
   )
