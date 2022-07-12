e40968eeb2d713ab8d74ad9905413f48ad50e875
dCieszynski <dCieszynski99@gmail.com>
2022-07-12 16:46:07 -0500

    change input types

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -34,22 +34,22 @@ const Searchbar: React.FC<Props> = ({ setSearchParams }) => {
 
   return (
     <div className="flex flex-col mt-4 w-full">
-      <div className="flex flex-col gap-2 border-2 p-4 rounded-2xl">
+      <form className="flex flex-col gap-2 border-2 p-4 rounded-2xl">
         <div className="flex justify-between">
           <label>MinLatitude</label>
-          <input className="w-1/4 text-black" type="text" ref={minLatInput} />
+          <input className="w-1/4 text-black" type="number" ref={minLatInput} />
         </div>
         <div className="flex justify-between">
           <label>MaxLatitude</label>
-          <input className="w-1/4 text-black" type="text" ref={maxLatInput} />
+          <input className="w-1/4 text-black" type="number" ref={maxLatInput} />
         </div>
         <div className="flex justify-between">
           <label>MinLongitude</label>
-          <input className="w-1/4 text-black" type="text" ref={minLngInput} />
+          <input className="w-1/4 text-black" type="number" ref={minLngInput} />
         </div>
         <div className="flex justify-between">
           <label>MaxLongitude</label>
-          <input className="w-1/4 text-black" type="text" ref={maxLngInput} />
+          <input className="w-1/4 text-black" type="number" ref={maxLngInput} />
         </div>
         <button
           className="self-center border-2 rounded-lg w-fit py-2 px-4"
@@ -57,7 +57,7 @@ const Searchbar: React.FC<Props> = ({ setSearchParams }) => {
         >
           Search
         </button>
-      </div>
+      </form>
     </div>
   );
 };
